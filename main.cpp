79af193dcf31a1006c5b1dc346d17e0ba80fd4c0
terramine21 <78859767+terramine21@users.noreply.github.com>
2021-05-12 21:08:12 +0300

    Update main.cpp
    Исправил функции arr_build_by_colomn() и apply_rule(), но хищников становится меньше, а количество жертв неизменяется

--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@
 #include <list>
 #include <time.h>
 
-size_t count = 0;
-void map_draw(png::image <png::rgb_pixel>& image, size_t height, size_t width, int** arr)
+void map_draw(png::image <png::rgb_pixel>& image, size_t height, size_t width, int** arr, size_t frame)
 {
     png::rgb_pixel color0 = png::rgb_pixel(0, 0, 0);
     png::rgb_pixel color1 = png::rgb_pixel(0, 255, 0);
     png::rgb_pixel color2 = png::rgb_pixel(255, 0, 0);
-
+    int a = 0, b = 0;
 
     for (png::uint_32 i = 0; i != image.get_width(); i++)
     {
@@ -19,13 +18,22 @@ void map_draw(png::image <png::rgb_pixel>& image, size_t height, size_t width, i
             if (arr[i][j] == 0)
                 image[i][j] = color0;
             if (arr[i][j] == 1)
+            {
                 image[i][j] = color1;
+                a++;
+            }
             if (arr[i][j] == 2)
+            {
                 image[i][j] = color2;
+                b++;
+            }
+            
         }
     }
-    image.write("out/palette" + std::to_string(count) + ".png");
-    count++;
+    if (frame % 50 == 0)
+    {
+        image.write("out/frame" + std::to_string(frame) + ".png"); std::cout << a << " " << b << std::endl;
+    }
 }
 
 class Arr_Collect
@@ -60,12 +68,12 @@ public:
     void arr_build_by_colomn(int** arr, const std::string& chain, size_t height, size_t width)
     {
         size_t count = 0;
-        for (int i = width - 1; i >= 0; i--)
+        for (int i = 0; i < height; i++)
         {
-            for (int j = height - 1; j >= height; j--)
+            for (int j = 0; j < width; j++)
             {
                 int KOSTIL = (int)chain[count] - 48;
-                arr[i][j] = KOSTIL;
+                arr[j][i] = KOSTIL;
                 count++;
             }
         }
@@ -78,7 +86,7 @@ public:
             for (int j = height - 1; j >= 0; j--)
             {
                 int KOSTIL = (int)chain[count] - 48;
-                arr[i][j] = KOSTIL;
+                arr[j][i] = KOSTIL;
                 count++;
             }
         }
@@ -94,7 +102,6 @@ public:
         else if (a == 3)
             arr_build_re_by_colomn(arr, chain, height, width);
     }
-
     int** arr_create(size_t x, size_t y)
     {
         int** arr = new int* [x];
@@ -121,11 +128,11 @@ public:
     {
         std::string str = {};
 
-        for (int x = 0; x < height; x++)
+        for (int i = 0; i < height; i++)
         {
-            for (int y = 0; y < width; y++)
+            for (int j = 0; j < width; j++)
             {
-                str += std::to_string(arr[x][y]);
+                str += std::to_string(arr[i][j]);
             }
         }
         return str;
@@ -134,11 +141,11 @@ public:
     {
         std::string str = {};
 
-        for (int x = height - 1; x >= 0; x--)
+        for (int i = height - 1; i >= 0; i--)
         {
-            for (int y = width - 1; y >= 0; y--)
+            for (int j = width - 1; j >= 0; j--)
             {
-                str += std::to_string(arr[x][y]);
+                str += std::to_string(arr[i][j]);
             }
         }
         return str;
@@ -147,11 +154,11 @@ public:
     {
         std::string str = {};
 
-        for (int x = 0; x < width; x++)
+        for (int i = 0; i < height; i++)
         {
-            for (int y = 0; y < height; y++)
+            for (int j = 0; j < width; j++)
             {
-                str += std::to_string(arr[y][x]);
+                str += std::to_string(arr[j][i]);
             }
         }
         return str;
@@ -160,11 +167,11 @@ public:
     {
         std::string str = {};
 
-        for (int x = width - 1; x >= 0; x--)
+        for (int i = height - 1; i >= 0; i--)
         {
-            for (int y = height - 1; y >= 0; y--)
+            for (int j = width - 1; j >= 0; j--)
             {
-                str += std::to_string(arr[y][x]);
+                str += std::to_string(arr[j][i]);
             }
         }
         return str;
@@ -193,61 +200,79 @@ public:
             {
                 std::string str1 = {};
                 for (; from <= i; from++)
-                {
                     str1 += chain[from];
-                }
                 chains.push_back(str1);
+                if(chain.size() - i > 3)
+                {
+                    int step = rand() % 3;
+                    i += step;
+                    str1 = {};
+                    for(; from <= i; from++)
+                        str1 += chain[from];
+                    chains.push_back(str1);
+                }
             }
         }
         return chains;
     }
-    void apply_rule(std::list <std::string>& chains, int p)
-    {
+    void apply_rule(std::list <std::string>& chains)
+    { //0 - nothing, 1 - grass, 2 - cow
         for (auto it = chains.begin(); it != chains.end(); it++)
         {
-            if (rand() % 10 < p)
+            int p = 2, q = 10, r = 1, rnd = rand() % 100 + 1;
+            if (p < rnd)
             {
-                if (*it == "01")
-                    *it = "11";
-
+                if (*it == "10")
+                    *it = "01";
+                else if (*it == "01")
+                    *it = "10";
+                else if (*it == "20")
+                    *it = "02";
+                else if (*it == "02")
+                    *it = "20";
                 else if (*it == "12")
-                    *it = "22"; 
-
-                else if (*it == "2")
-                    *it = "0";
-
-                else if (*it == "1")
-                    *it = "0";
+                    *it = "21";
+                else if (*it == "21")
+                    *it = "12";
             }
+            else if (q < rnd)
+            {
+                if (*it == "10")
+                    *it = "11";
+                else if (*it == "12")
+                    *it = "22";
+            }
+            else if(r < rnd)
+            if (*it == "2")
+                *it = "0";
+               
         }
     }
 };
 
 int main()
 {
-
     srand(time(NULL));
-    int p = 5; //вероятность в % * 10
+    int p = 5; //вероятность деления строки в % * 10 //нету в презентаии
     const int width = 100, height = 100;
     png::image<png::rgb_pixel> image(height, width);
-
-
+    
     Arr_Collect arr_collect;
     int** arr = arr_collect.arr_create(height, width);
     arr_collect.arr_fill(arr, height, width);
 
-    map_draw(image, height, width, arr); //первый кадр
+    map_draw(image, height, width, arr, 0); //first frame
 
     Chain_Build chain_build;
 
     
-    for (int frame = 0; frame < 100 ; frame++)
+    for (int frame = 1; frame < 1000  ; frame++)
     {
         int a = rand() % 4;
         std::string chain = chain_build.str_create(arr, height, width, a);
         //std::cout << chain << std::endl;
         std::list<std::string> chains = chain_build.chains_breaker(chain, p);
-        chain_build.apply_rule(chains, p);
+        chain_build.apply_rule(chains);
 
         chain = {};
         for (auto it = chains.begin(); it != chains.end(); it++)
@@ -256,7 +281,7 @@ int main()
         }
         arr_collect.arr_build(arr, chain, height, width, a);
 
-        map_draw(image, height, width, arr); //второй кадр
+        map_draw(image, height, width, arr, frame); //второй кадр
     }
 
     for (int i = 0; i < height; i++)
